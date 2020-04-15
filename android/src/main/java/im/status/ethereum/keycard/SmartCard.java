260153dccb9340ab33d6f1b79c0bc4a5bf9735ac
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2020-04-15 18:19:00 +0300

    +

--- android/src/main/java/im/status/ethereum/keycard/SmartCard.java
+++ android/src/main/java/im/status/ethereum/keycard/SmartCard.java
@@ -548,8 +548,6 @@ public class SmartCard extends BroadcastReceiver implements CardListener {
 
     public String sign(final String pairingBase64, final String pin, final String message) throws IOException, APDUException {
         KeycardCommandSet cmdSet = new KeycardCommandSet(this.cardChannel);
-        KeyPath currentPath = new KeyPath(cmdSet.getStatus(KeycardCommandSet.GET_STATUS_P1_KEY_PATH).checkOK().getData());
-        Log.i(TAG, "Signing with current key path: " + currentPath);
         cmdSet.select().checkOK();
 
         Pairing pairing = new Pairing(pairingBase64);
@@ -557,6 +555,9 @@ public class SmartCard extends BroadcastReceiver implements CardListener {
 
         cmdSet.autoOpenSecureChannel();
         Log.i(TAG, "secure channel opened");
+
+        KeyPath currentPath = new KeyPath(cmdSet.getStatus(KeycardCommandSet.GET_STATUS_P1_KEY_PATH).checkOK().getData());
+        Log.i(TAG, "Signing with current key path: " + currentPath);
 
         cmdSet.verifyPIN(pin).checkOK();
         Log.i(TAG, "pin verified");
