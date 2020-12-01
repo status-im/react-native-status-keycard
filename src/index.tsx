c944aa7f2b29820851ef1d684ac4bf75a7155970
Andrea Franz <andrea@gravityblast.com>
2020-12-01 13:46:07 +0100

    remove unused files

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type StatusKeycardType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { StatusKeycard } = NativeModules;
-
-export default StatusKeycard as StatusKeycardType;
