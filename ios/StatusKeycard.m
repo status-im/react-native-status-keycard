b3d56547fbbbeab09295a724dc1a48811459335e
Andrea Franz <andrea@gravityblast.com>
2020-12-02 19:21:21 +0100

    add nfcIsEnabled definition

--- ios/StatusKeycard.m
+++ ios/StatusKeycard.m
@@ -3,6 +3,7 @@
 @interface RCT_EXTERN_REMAP_MODULE(RNStatusKeycard, StatusKeycard, NSObject)
 
 RCT_EXTERN_METHOD(nfcIsSupported:(RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(nfcIsEnabled:(RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(openNfcSettings:(RCTPromiseResolveBlock)resolve rejecter: (RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(init:(NSString *)pin resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(pair:(NSString *)pairingPassword resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
